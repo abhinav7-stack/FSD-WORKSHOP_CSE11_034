d1d186c98a78f4676c801f701132326879b840e2
abhinav7-stack <abhinav.25b01011114@abes.ac.in>
2026-09-18 14:50:56 +0530

    18 September Assignment4

--- FRONT-END/src/main.jsx
+++ FRONT-END/src/main.jsx
@@ -1,11 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import home from './componets/home.jsx'
+// // import { StrictMode } from 'react'
+// // import { createRoot } from 'react-dom/client'
+// // import './index.css'
+// // import App from './App.jsx'
+// // import home from './componets/home.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// // createRoot(document.getElementById('root')).render(
+// //   <StrictMode>
+// //     <App />
+// //   </StrictMode>,
+// // )
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import ApiTester from './components/api_tester.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <ApiTester />
+//   </StrictMode>,
+// )
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import ApiTester from "./components/api_tester.jsx";
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <ApiTester />
+    </StrictMode>
+);
--- back-end/lab6/express_server.js
+++ back-end/lab6/express_server.js
@@ -314,8 +314,9 @@
 
 
 import express from "express";
-
+import cors from "cors" ;
 const app = express();
+app.use(cors());
 const port = 3000;
 
 app.use(express.json());
